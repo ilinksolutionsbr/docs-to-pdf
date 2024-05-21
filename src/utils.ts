bb8b31353322a294f492beaae865a3876fd2196c
Wester Torres <wester.torres9@gmail.com>
2024-05-21 09:22:16 -0300

    chore(deps): customize layout

--- src/utils.ts
+++ src/utils.ts
@@ -290,13 +290,8 @@ export function generateTocHtml(headers: any[], tocTitle?: string) {
     .map(
       (header) =>
         `<li class="toc-item toc-item-${header.level}" style="margin-left:${
-          (header.level - 1) * 10
-        }px"><a href="#${header.id}">
-        <span>${header.level == 2 ? '':
-          header.level == 3 ? '......':
-          '............'
-        }</span>
-        ${header.header}</a></li>`,
+          (header.level - 1) * 20
+        }px"><a href="#${header.id}">${header.header}</a></li>`,
     )
     .join('\n');
   // Return the HTML code for the table of contents
